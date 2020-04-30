9007672e59c09ac32808ced2eab209f5fde4e600
Jean-Richard Latortue <rickyransomco@gmail.com>
2020-04-30 22:24:48 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,6 +1,7 @@
-arr = 1, 2, 3, 4, 5
-arr2 = 10, 20, 30, 40, 50
-arr3 = arr.concat(arr2)
-def using_concat (array1,array2)
+
+def using_concat (arr1,arr2)
+  arr1 = 1, 2, 3, 4, 5
+  arr2 = 10, 20, 30, 40, 50
+  arr3 = arr1.concat(arr2)
   puts arr3
 end
